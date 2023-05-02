9faad28f4986f4860136d6bf8ebb79589e6e8643
SharleneNdinda <sharlenendinda@gmail.com>
2023-05-02 22:00:57 +0300

    Add application configs

--- get_it_done/cli.py
+++ get_it_done/cli.py
@@ -7,6 +7,7 @@ app = typer.Typer()
 
 
 def _version_callback(value: bool) -> None:
+    """Shows app version."""
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
--- tests/test_get_it_done.py
+++ tests/test_get_it_done.py
@@ -5,6 +5,7 @@ from get_it_done import __app_name__, __version__, cli
 runner = CliRunner()
 
 def test_version():
+    """Test the app version."""
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
